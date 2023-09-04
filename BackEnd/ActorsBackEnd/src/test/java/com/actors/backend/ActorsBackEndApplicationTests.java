9bd8e7daaf8ea4dc67de7560cb9ddaef7c283910
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-09-04 15:12:31 -0700

    Created BackEnd with Spring Boot
    Included the credentials and Hibernate properties.

---
+++ BackEnd/ActorsBackEnd/src/test/java/com/actors/backend/ActorsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actors.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
