9d52fad3ebf700b0ac232375ad09a49ca0b755cb
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-09-05 07:58:10 -0700

    Created RESTful API class and fixed Hibernate naming strategy
    I provided two links to view the problem and solution, due to updates made after Hibernate 6.

--- BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/ActorsBackEndApplication.java
+++ BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/ActorsBackEndApplication.java
@@ -9,5 +9,8 @@ public class ActorsBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ActorsBackEndApplication.class, args);
 	}
-
+	
+	/* Link 1 for resolving name as strategy Hibernate: https://discourse.hibernate.org/t/unable-to-resolve-name-org-hibernate-dialect-mysqlinnodbdialect-as-strategy-org-hibernate-dialect-dialect-with-hibernate-6-2/7936
+	 * Link 2 for references: https://docs.jboss.org/hibernate/orm/6.0/migration-guide/migration-guide.html#_dialects
+	*/
 }
--- BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/controller/ActorController.java
+++ BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/controller/ActorController.java
@@ -1,5 +1,28 @@
 package com.actors.backend.controller;
 
-public class ActorController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.actors.backend.model.Actor;
+import com.actors.backend.repository.ActorRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class ActorController {
+	
+	@Autowired
+	private ActorRepository actorRepo;
+
+	
+	//REST API to get all actors
+	@GetMapping("/actors")
+	public List<Actor> getAllActors() {
+		return actorRepo.findAll();
+	}
+	
+	
 }
