93aa986cf4d6000e971801eedde4eb05d6f3059f
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-09-05 13:50:53 -0700

    Created Add Actors API on Backend

--- BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/controller/ActorController.java
+++ BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/controller/ActorController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.actors.backend.model.Actor;
 import com.actors.backend.repository.ActorRepository;
+
+import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -26,5 +30,9 @@ public class ActorController {
 		return actorRepo.findAll();
 	}
 	
-	
+	//Create actor REST API
+	@PostMapping("/actors")
+	public Actor createActor(@Valid @RequestBody Actor actor) {
+		return actorRepo.save(actor);
+	}
 }
