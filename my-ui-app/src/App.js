81928cd367dbdf75374bbdc250828a1fa6809f4d
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-09-05 13:42:35 -0700

    Configured Routing for home page

--- my-ui-app/src/App.js
+++ my-ui-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
 import ListActorsComponent from './components/ListActorsComponent';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -11,13 +11,18 @@ function App() {
   return (
 
     <div>
+      <BrowserRouter>
+          <Header/>
 
-      <Header/>
-      <div className='container'>
-        <ListActorsComponent/>
-      </div>
-      <Footer/>
-      
+            <div className='container'>
+                <Switch>
+                    <Route path='/' Component={ListActorsComponent}></Route>
+                    <Route path='/actors' Component={ListActorsComponent}></Route>
+                </Switch>
+            </div>
+
+          <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
