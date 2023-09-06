73055be75d54489d18bb517b4d8ff94d1e4437cc
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-09-06 14:34:54 -0700

    Created getActorByID REST API on backend

--- BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/controller/ActorController.java
+++ BackEnd/ActorsBackEnd/src/main/java/com/actors/backend/controller/ActorController.java
@@ -3,13 +3,16 @@ package com.actors.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.actors.backend.exception.ResourceNotFoundException;
 import com.actors.backend.model.Actor;
 import com.actors.backend.repository.ActorRepository;
 
@@ -34,5 +37,16 @@ public class ActorController {
 	@PostMapping("/actors")
 	public Actor createActor(@Valid @RequestBody Actor actor) {
 		return actorRepo.save(actor);
+	}
+	
+	//Get actor by ID
+	@GetMapping("/actors/{id}")
+	public ResponseEntity<Actor> getActorByID(@PathVariable("id") Long id) throws ResourceNotFoundException {
+		
+		Actor actorObj = actorRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Actor is not found with id: " + id));
+		
+		
+		return ResponseEntity.ok(actorObj);
 	}
 }
