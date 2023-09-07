cdf77a8675276f873d18474b396cac818b67b73d
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-09-07 14:33:24 -0700

    Finished displaying the ViewActorDetails component

--- my-ui-app/src/components/ViewActorDetails.js
+++ my-ui-app/src/components/ViewActorDetails.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import ActorsService from '../services/ActorsService';
 
 class ViewActorDetails extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            id: this.props.match.params.id,
+            actor: {}
         };
+    }
+
+    componentDidMount() {
+        ActorsService.getActorByID(this.state.id).then( response => {
+            this.setState({
+                actor: response.data
+            })
+        });
     }
 
     render() {
         return (
             <div className='container'>
-                <h2>The Actor's Details</h2>
+                <div className='card col-md-6 offset-md-3'>
+                    <h3 className='text-center'>View Actor's Details</h3>
+
+                    <div className='card-body'>
+                        <div className='row'>
+                            <label>First Name: </label>
+                            <div>{ this.state.actor.firstName }</div>
+                        </div>
+                        <div className='row'>
+                            <label>Last Name: </label>
+                            <div>{ this.state.actor.lastName }</div>
+                        </div>
+                        <div className='row'>
+                            <label>Actor's Email: </label>
+                            <div>{ this.state.actor.emailId }</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
