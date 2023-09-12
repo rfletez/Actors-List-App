847d78b18922ef3b6fa75cfbfc34c97954566209
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-09-12 08:43:24 -0700

    Modified changes on backend to application.properties and pom.xml

--- BackEnd/LoginAndRegistration/src/main/java/com/users/login/service/UserServiceImpl.java
+++ BackEnd/LoginAndRegistration/src/main/java/com/users/login/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.users.login.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.users.login.dto.LoginDTO;
